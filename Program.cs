ab5977f828381c8a866ca37de534a6a50a173cb9
HYDlink <508860736@qq.com>
2023-03-24 18:03:36 +0800

    feature: Add basic image load write, structure parsing

--- Program.cs
+++ Program.cs
@@ -1,71 +1,90 @@
-﻿// Copyright (C) 2016 Maxim Gumin, The MIT License (MIT)
-
 using System;
-using System.Xml.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Processors.Transforms;
 
-static class Program
-{
-    static void Main()
-    {
-        Stopwatch sw = Stopwatch.StartNew();
-        var folder = System.IO.Directory.CreateDirectory("output");
-        foreach (var file in folder.GetFiles()) file.Delete();
+// namespace WaveFunctionCollapse;
 
-        Random random = new();
-        XDocument xdoc = XDocument.Load("samples.xml");
 
-        foreach (XElement xelem in xdoc.Root.Elements("overlapping", "simpletiled"))
+var name = "Knots";
+var fileName = $"tilesets/{name}.xml";
+// var xml = File.ReadAllText(fileName);
+var xroot = XDocument.Load(fileName).Root;
+var set = new TileSet(
+    xroot.Element("tiles").Descendants()
+        .Select(tile =>
         {
-            Model model;
-            string name = xelem.Get<string>("name");
-            Console.WriteLine($"< {name}");
-
-            bool isOverlapping = xelem.Name == "overlapping";
-            int size = xelem.Get("size", isOverlapping ? 48 : 24);
-            int width = xelem.Get("width", size);
-            int height = xelem.Get("height", size);
-            bool periodic = xelem.Get("periodic", false);
-            string heuristicString = xelem.Get<string>("heuristic");
-            var heuristic = heuristicString == "Scanline" ? Model.Heuristic.Scanline : (heuristicString == "MRV" ? Model.Heuristic.MRV : Model.Heuristic.Entropy);
-
-            if (isOverlapping)
+            var name = tile.Attribute("name").Value;
+            var strings = name.Split(' ', 2);
+            var (rawName, rotate) = strings switch
             {
-                int N = xelem.Get("N", 3);
-                bool periodicInput = xelem.Get("periodicInput", true);
-                int symmetry = xelem.Get("symmetry", 8);
-                bool ground = xelem.Get("ground", false);
+                [{ } s1, { } s2] => (s1, s2),
+                [{ } s3] => (s3, "0"),
+                _ => throw new Exception("Invalid Name"),
+            };
+            var symmetry = Symmetry.TryParse<Symmetry>(tile.Attribute("symmetry").Value, out var s) ? s : default;
+            return new Tile(rawName, symmetry, rotate);
+        }).ToList(),
+    xroot.Element("neighbors").Descendants()
+        .Select(neighbor => new Neighbor(neighbor.Attribute("left").Value, neighbor.Attribute("right").Value)).ToList(),
+    // null
+    xroot.Element("subsets").Descendants()
+        .Select(s => new Subset(
+            s.Attribute("name").Value,
+            s.Descendants().Select(d => d.Attribute("name").Value).ToList())).ToList()
+);
 
-                model = new OverlappingModel(name, N, width, height, periodicInput, periodic, symmetry, ground, heuristic);
-            }
-            else
-            {
-                string subset = xelem.Get<string>("subset");
-                bool blackBackground = xelem.Get("blackBackground", false);
+string TileFile(string name, string tileName) => $"tilesets/{name}/{tileName}.png";
 
-                model = new SimpleTiledModel(name, subset, width, height, periodic, blackBackground, heuristic);
-            }
+using var image = Image.Load<Bgra32>(TileFile(name, "corner"));
+var tile_width = image.Width;
+var tile_height = image.Height;
+image.Mutate(m => m.Resize(tile_width * 4, image.Height * 4, new NearestNeighborResampler()));
 
-            for (int i = 0; i < xelem.Get("screenshots", 2); i++)
-            {
-                for (int k = 0; k < 10; k++)
-                {
-                    Console.Write("> ");
-                    int seed = random.Next();
-                    bool success = model.Run(seed, xelem.Get("limit", -1));
-                    if (success)
-                    {
-                        Console.WriteLine("DONE");
-                        model.Save($"output/{name} {seed}.png");
-                        if (model is SimpleTiledModel stmodel && xelem.Get("textOutput", false))
-                            System.IO.File.WriteAllText($"output/{name} {seed}.txt", stmodel.TextOutput());
-                        break;
-                    }
-                    else Console.WriteLine("CONTRADICTION");
-                }
-            }
-        }
 
-        Console.WriteLine($"time = {sw.ElapsedMilliseconds}");
-    }
+var curTile = set.Tiles.First();
+var rightTiles = set.Tiles;
+using var output_img = new Image<Bgra32>(tile_width * 8, tile_height * 4, Color.White);
+output_img.Mutate(ctx => ctx.DrawImage(image, new Point(0, 0), 1f));
+foreach (var (i, rightTile) in rightTiles.Select((t,i) => (i, t.Name)))
+{
+    var x = tile_width * 4 + tile_width * (i % 4);
+    var y = tile_height * (i / 4);
+    using var neighbor = Image.Load<Bgra32>(TileFile(name, rightTile));
+    output_img.Mutate(ctx => ctx.DrawImage(neighbor, new Point(x, y), 1f));
 }
+Console.WriteLine(set.Tiles.Count);
+
+var folder = Directory.CreateDirectory("output");
+foreach (var file in folder.GetFiles()) file.Delete();
+var outputCornerX4Png = $"output\\corner_x4.png";
+output_img.Save(outputCornerX4Png, new PngEncoder());
+Process.Start(new ProcessStartInfo(outputCornerX4Png) {UseShellExecute = true});
+
+public enum Symmetry
+{
+    L,
+    I,
+    X,
+    T,
+    [Description("\\")] Slash,
+}
+
+public record Tile(string Name, Symmetry Symmetry, string Rotate);
+
+public record Neighbor(string Left, string Right);
+
+public record Subset(string Name, List<string> Tiles);
+
+public record TileSet(List<Tile> Tiles, List<Neighbor> Neighbors, List<Subset> Subsets);
